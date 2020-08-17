814a683928180a2db576d58d83eb7d403b1b40b3
ivan.ovcharenko <ovcharenkoivan@hotmail.com>
2020-08-17 15:10:54 +0300

    icon added

--- HAS/Form1.Designer.cs
+++ HAS/Form1.Designer.cs
@@ -28,13 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(843, 487);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Heaters Accounting System";
             this.ResumeLayout(false);
